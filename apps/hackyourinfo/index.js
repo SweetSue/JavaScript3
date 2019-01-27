6f929ce1d2d593d76d56dd18047c29a91c86b7eb
Joost Lubach <joostlubach@gmail.com>
2019-01-27 11:38:18 +0100

    Revert "App hackyourinfo: Fixed small bug"
    This reverts commit 5553de8adc745b66948a9f6945226a0b2c620a15.

--- apps/hackyourinfo/index.js
+++ apps/hackyourinfo/index.js
@@ -15,13 +15,12 @@ app.use(cors())
 app.post('/:name.json', async (request, response) => {
   try {
     const filename = path.join(infoDir, `${request.params.name}.json`)
-    await fs.ensureDir(infoDir)
     await fs.writeFile(filename, JSON.stringify(request.body, null, 2))
     response.statusCode = 200
-    response.json({ ok: true })
+    response.json({ok: true})
   } catch (error) {
     response.statusCode = 500
-    response.json({ error: "An error occurred" })
+    response.json({error: "An error occurred"})
     console.error(error)
   }
 
@@ -30,7 +29,6 @@ app.post('/:name.json', async (request, response) => {
 
 app.get('/_all.json', async (request, response) => {
   try {
-    await fs.ensureDir(infoDir)
     const files = await fs.readdir(infoDir)
     const promises = files.map(file => fs.readFile(path.join(infoDir, file), 'utf8').then(raw => [file, raw]))
     const raws = await Promise.all(promises)
@@ -42,7 +40,7 @@ app.get('/_all.json', async (request, response) => {
     response.json(result)
   } catch (error) {
     response.statusCode = 500
-    response.json({ error: "An error occurred" })
+    response.json({error: "An error occurred"})
     console.error(error)
   }
   response.end()
@@ -51,16 +49,15 @@ app.get('/_all.json', async (request, response) => {
 app.get('/:name.json', async (request, response) => {
   try {
     const filename = path.join(infoDir, `${request.params.name}.json`)
-    await fs.ensureDir(infoDir)
     const raw = await fs.readFile(filename, 'utf8')
     response.json(JSON.parse(raw))
   } catch (error) {
     if (error.code === 'ENOENT') {
       response.statusCode = 404
-      response.json({ error: "File not found" })
+      response.json({error: "File not found"})
     } else {
       response.statusCode = 500
-      response.json({ error: "An error occurred" })
+      response.json({error: "An error occurred"})
       console.error(error)
     }
   }
@@ -70,10 +67,10 @@ app.get('/:name.json', async (request, response) => {
 app.delete('/_all.json', async (request, response) => {
   try {
     await fs.emptyDir(infoDir)
-    response.json({ ok: true })
+    response.json({ok: true})
   } catch (error) {
     response.statusCode = 500
-    response.json({ error: "An error occurred" })
+    response.json({error: "An error occurred"})
     console.error(error)
   }
   response.end()
@@ -81,16 +78,15 @@ app.delete('/_all.json', async (request, response) => {
 
 app.delete('/:name.json', async (request, response) => {
   try {
-    await fs.ensureDir(infoDir)
     await fs.unlink(path.join(infoDir, `${request.params.name}.json`))
-    response.json({ ok: true })
+    response.json({ok: true})
   } catch (error) {
     if (error.code !== 'ENOENT') {
       response.statusCode = 500
-      response.json({ error: "An error occurred" })
+      response.json({error: "An error occurred"})
       console.error(error)
     } else {
-      response.json({ ok: true })
+      response.json({ok: true})
     }
   }
   response.end()
@@ -98,7 +94,7 @@ app.delete('/:name.json', async (request, response) => {
 
 app.use((request, response) => {
   response.statusCode = 404
-  response.json({ error: "Not found" })
+  response.json({error: "Not found"})
   response.end()
 })
 
